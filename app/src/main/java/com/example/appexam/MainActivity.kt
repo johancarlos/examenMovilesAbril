eef8a9e8140beb51386d1a7da25878c12c0c1039
johancarlos <johancarlos.bo@gmail.com>
2020-04-30 21:54:01 -0400

    alert

--- app/src/main/java/com/example/appexam/MainActivity.kt
+++ app/src/main/java/com/example/appexam/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.appexam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.LoginFilter
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
@@ -31,8 +33,27 @@ class MainActivity : AppCompatActivity() {
     private fun validarCreate(resp:Boolean){
         if(resp == true){
             Toast.makeText(this,"Creado Exitosamente",Toast.LENGTH_LONG).show()
+            val alertDialog = AlertDialog.Builder(this)
+                .setTitle("Create User")
+                .setMessage("Creado Exitosamente!")
+                .setPositiveButton("Cerrar", null)
+                .create()
+            alertDialog.show()
+            val positiveButton: Button =
+                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            positiveButton.setOnClickListener {
+                recreate();
+            }
         }else{
             Toast.makeText(this,"Error",Toast.LENGTH_LONG).show()
+            val alertDialog = AlertDialog.Builder(this)
+                .setTitle("Create User")
+                .setMessage("Error al crear!")
+                .setPositiveButton("Cerrar", null)
+                .create()
+            alertDialog.show()
+            val positiveButton: Button =
+                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
         }
     }
 
